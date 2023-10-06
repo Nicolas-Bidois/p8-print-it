d4ea17ecdae4bd868f5f7f2050004495d0a9bbb6
Nicolas Bidois <nicolasbidois2@gmail.com>
2023-10-06 10:17:35 +0200

    commit du projet

---
+++ script.js
@@ -0,0 +1,10 @@
+const montableau = ["Cachalot", "Pétunia", "Serviette"]
+let score = 0
+
+let motutilisaterur = prompt ("entrer le mot : " + montableau[0])
+
+if (motutilisaterur ===montableau[0]) {
+    score++
+ 
+}
+console.log(score);
